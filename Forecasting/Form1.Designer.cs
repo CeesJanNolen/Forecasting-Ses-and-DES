40663c385f520ba0661446fdaf4c40f565fad1ee
Cees-Jan <ceesjannolen@live.nl>
2017-04-06 16:05:46 +0200

    initial commit

---
+++ Forecasting/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace Forecasting
+{
+  partial class Form1
+  {
+	/// <summary>
+	/// Required designer variable.
+	/// </summary>
+	private System.ComponentModel.IContainer components = null;
+
+	/// <summary>
+	/// Clean up any resources being used.
+	/// </summary>
+	/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+	protected override void Dispose(bool disposing)
+	{
+	  if (disposing && (components != null))
+	  {
+		components.Dispose();
+	  }
+	  base.Dispose(disposing);
+	}
+
+	#region Windows Form Designer generated code
+
+	/// <summary>
+	/// Required method for Designer support - do not modify
+	/// the contents of this method with the code editor.
+	/// </summary>
+	private void InitializeComponent()
+	{
+	  this.components = new System.ComponentModel.Container();
+	  this.zedGraphControl1 = new ZedGraph.ZedGraphControl();
+	  this.checkBox1 = new System.Windows.Forms.CheckBox();
+	  this.SuspendLayout();
+	  // 
+	  // zedGraphControl1
+	  // 
+	  this.zedGraphControl1.IsShowPointValues = true;
+	  this.zedGraphControl1.Location = new System.Drawing.Point(50, 50);
+	  this.zedGraphControl1.Name = "zedGraphControl1";
+	  this.zedGraphControl1.Size = new System.Drawing.Size(10, 71);
+	  this.zedGraphControl1.TabIndex = 0;
+	  this.zedGraphControl1.UseExtendedPrintDialog = true;
+	  // 
+	  // checkBox1
+	  // 
+	  this.checkBox1.AutoSize = true;
+	  this.checkBox1.Location = new System.Drawing.Point(168, 121);
+	  this.checkBox1.Name = "checkBox1";
+	  this.checkBox1.Size = new System.Drawing.Size(98, 21);
+	  this.checkBox1.TabIndex = 0;
+	  this.checkBox1.Text = "checkBox1";
+	  this.checkBox1.UseVisualStyleBackColor = true;
+	  // 
+	  // Form1
+	  // 
+	  this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+	  this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+	  this.ClientSize = new System.Drawing.Size(662, 374);
+	  this.Controls.Add(this.checkBox1);
+	  this.Name = "Form1";
+	  this.Text = "Form1";
+	  this.Load += new System.EventHandler(this.Form1_Load);
+	  this.ResumeLayout(false);
+	  this.PerformLayout();
+
+	}
+
+	private ZedGraph.ZedGraphControl zedGraphControl1;
+	#endregion
+
+	private System.Windows.Forms.CheckBox checkBox1;
+  }
+}
+
---
+++ Forecasting/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ZedGraph;
+
+namespace Forecasting
+{
+  public partial class Form1 : Form
+  {
+	public Form1()
+	{
+	  InitializeComponent();
+	}
+
+	private void Form1_Load(object sender, EventArgs e)
+	{
+	  CreateGraph(zedGraphControl1, "hoi", "as", "asd"); 
+	}
+
+	public void CreateGraph(ZedGraphControl zgc, string Title, string Xas, string Yas)
+	{
+	  //add reference
+	  GraphPane MyGraph = zgc.GraphPane;
+
+	  //add the titles to the graph
+	  MyGraph.Title.Text = Title;
+	  MyGraph.XAxis.Title.Text = Xas;
+	  MyGraph.YAxis.Title.Text = Yas;
+
+	  double x;
+	  double y1;
+	  double y2;
+
+	  PointPairList List1 = new PointPairList();
+	  PointPairList List2 = new PointPairList();
+
+	  for (int i = 0; i < 10; i++)
+	  {
+		x = (double)0 + i;
+		y1 = x * x;
+		y2 = x * 2;
+
+		List1.Add(x, y1);
+		List2.Add(x, y2);
+	  }
+
+	  MyGraph.AddCurve("x^2", List1, Color.Blue, SymbolType.Square);
+	  MyGraph.AddCurve("2x", List2, Color.Red, SymbolType.Star);
+
+	  zgc.AxisChange();
+
+	}
+  }
+}
